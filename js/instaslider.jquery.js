e11f1c90537313b6fc14fc9af3b056de79509948
Chris Till <christill89@gmail.com>
2013-05-17 11:47:52 +0100

    fixed indentation and lint errors

--- js/instaslider.jquery.js
+++ js/instaslider.jquery.js
@@ -26,138 +26,140 @@
 //		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //		THE SOFTWARE.
 //
-
 // Polyfill for older browsers
-if (typeof Object.create !== 'function'){
-	Object.create = function(obj){
-		function F(){};
-		F.prototype = obj;
-		return new F();
-	}
+if (typeof Object.create !== 'function') {
+    Object.create = function (obj) {
+        "use strict";
+
+        function F() {}
+        F.prototype = obj;
+        return new F();
+    };
 }
 
-(function($, window, document, undefined){
+(function ($, window, document, undefined) {
+    "use strict";
 
-	var InstaSlider = {
+    var InstaSlider = {
 
-		init: function(options, container){
+        init: function (options, container) {
 
-			var self = this;
-				self.container = container,
-				self.$container = $(container),
-				self.current = 0, // Set current to 0 on initialise
-				self.imgWidth = self.$container.width(), // img width will be the same as the container
-				self.options = $.extend({}, $.fn.instaSlider.options, options),
-				self.endpoint = 'https://api.instagram.com/v1/tags/' + this.options.hash + '/media/recent?client_id=' + this.options.clientID;	
-				
-			this.createSlider(); // Create the slider
+            var self = this;
+            self.container = container,
+            self.$container = $(container),
+            self.current = 0, // Set current to 0 on initialise
+            self.imgWidth = self.$container.width(), // img width will be the same as the container
+            self.options = $.extend({}, $.fn.instaSlider.options, options),
+            self.endpoint = 'https://api.instagram.com/v1/tags/' + this.options.hash + '/media/recent?client_id=' + this.options.clientID;
 
-			self.sliderUL = self.$container.find('.instaslider-wrapper ul');
-		},
-		
-		createSlider: function(){
-			// create the slider
-			var slider = this.$container.append('<div class="instaslider-wrapper"><ul></ul></div>');
-			
-			this.createNav();
-			this.createSlides();
-		},
+            this.createSlider(); // Create the slider
 
-		createNav: function() {
-			var self = this;
-			// create the navigation for the slider
-			var	buttonPrev = '<button class="' + this.options.prevClass + '" data-direction="prev">Prev</button>',
-				buttonNext = '<button class="' + this.options.nextClass + '" data-direction="next">Next</button>',
-				nav = '<div class="instaslider-nav">' + buttonPrev + buttonNext + '</div>';
+            self.sliderUL = self.$container.find('.instaslider-wrapper ul');
+        },
 
-			// append it to the container
-			this.$container.append(nav);
+        createSlider: function () {
+            // create the slider
+            this.$container.append('<div class="instaslider-wrapper"><ul></ul></div>');
 
-			// when a button is clicked set current
-			this.$container.find('button').on('click', function(){
-				self.setCurrent( $(this).data('direction') );
-				self.transition();
-			});
-		},
+            this.createNav();
+            this.createSlides();
+        },
 
-		fetch: function() {
-			//fetch images from instagram
-			return $.ajax({
-				url: this.endpoint,
-				data: {},
-				dataType: 'jsonp',
-				type:'GET'
-			});
-		},
+        createNav: function () {
+            var self = this;
+            // create the navigation for the slider
+            var buttonPrev = '<button class="' + this.options.prevClass + '" data-direction="prev">Prev</button>',
+                buttonNext = '<button class="' + this.options.nextClass + '" data-direction="next">Next</button>',
+                nav = '<div class="instaslider-nav">' + buttonPrev + buttonNext + '</div>';
 
-		createSlides: function() {
-			// create the slides
-			var self = this,
-				container = this.$container,
-				sliderUL = container.find('.instaslider-wrapper ul');			
-			
-			self.fetch().done(function(results){
-				// Limit the amount of results
-				results = self.limit( results.data, self.options.limit );
-				// loop over results create a slider for each one.
-				self.slides = $.map(results, function(obj, i){
-					var img = '<li><img src="' + results[i].images.standard_resolution.url + '" /></li>';
-					sliderUL.append(img);
-				});
-			});
+            // append it to the container
+            this.$container.append(nav);
 
-			self.fetch().fail(function(){
-				sliderUL.remove();
-				container.html('<div class="error"><p>Sorry,<br /> Could not fetch images at this time.</p></div>');
-			})
+            // when a button is clicked set current
+            this.$container.find('button').on('click', function () {
+                self.setCurrent($(this).data('direction'));
+                self.transition();
+            });
+        },
 
-		},
+        fetch: function () {
+            //fetch images from instagram
+            return $.ajax({
+                url: this.endpoint,
+                data: {},
+                dataType: 'jsonp',
+                type: 'GET'
+            });
+        },
 
-		setCurrent: function(direction) {
-			// set the current slide and handle direction here
-			var self = this;
-			var pos = self.current;
-			pos += ( ~~(direction === 'next') || -1);
-			self.current = ( pos < 0 ) ? self.options.limit -1 : pos % self.options.limit;
-			return pos;
-		},
+        createSlides: function () {
+            // create the slides
+            var self = this,
+                container = this.$container,
+                sliderUL = container.find('.instaslider-wrapper ul');
 
-		transition: function() {
-			// handle animation and slide transition here
-			var self = this;
+            self.fetch().done(function (results) {
+                // Limit the amount of results
+                results = self.limit(results.data, self.options.limit);
+                // loop over results create a slider for each one.
+                self.slides = $.map(results, function (obj, i) {
+                    var img = '<li><img src="' + results[i].images.standard_resolution.url + '" /></li>';
+                    sliderUL.append(img);
+                });
+            });
 
-			self.sliderUL.stop().animate({
-				'margin-left': -( this.current * this.imgWidth )
-			}, self.options.duration);
-		},
+            self.fetch().fail(function () {
+                sliderUL.remove();
+                container.html('<div class="error"><p>Sorry,<br /> Could not fetch images at this time.</p></div>');
+            });
 
-		limit: function(obj, limit) {
-			return obj.slice( 0, limit );
-		},
+        },
 
-	};
-	
-	$.fn.instaSlider = function(options){
-		return this.each(function(){
-			var instaSlider = Object.create( InstaSlider );
-			instaSlider.init(options, this);
-			$.data(this, 'instaSlider', instaSlider);
-		});
-	};
+        setCurrent: function (direction) {
+            // set the current slide and handle direction here
+            var self = this;
+            var pos = self.current;
+            pos += (~~(direction === 'next') || -1);
+            self.current = (pos < 0) ? self.options.limit - 1 : pos % self.options.limit;
+            return pos;
+        },
 
-	/*----------------------------------------------------------------
+        transition: function () {
+            // handle animation and slide transition here
+            var self = this;
+
+            self.sliderUL.stop().animate({
+                'margin-left': -(this.current * this.imgWidth)
+            }, self.options.duration);
+        },
+
+        limit: function (obj, limit) {
+            return obj.slice(0, limit);
+        },
+
+    };
+
+    $.fn.instaSlider = function (options) {
+        return this.each(function () {
+            var instaSlider = Object.create(InstaSlider);
+            instaSlider.init(options, this);
+            $.data(this, 'instaSlider', instaSlider);
+        });
+    };
+
+    /*----------------------------------------------------------------
 		Default Options
 	----------------------------------------------------------------*/
 
-	$.fn.instaSlider.options = {
+    $.fn.instaSlider.options = {
 
-		// Default Options
-		clientID: null,
-		hash:'photooftheday',
-		prevClass: 'prev',
-		nextClass: 'next',
-		limit: 5,
-		duration: 400
-	}
+        // Default Options
+        clientID: null,
+        hash: 'photooftheday',
+        prevClass: 'prev',
+        nextClass: 'next',
+        limit: 5,
+        duration: 400
+    };
 
 })(jQuery, window, document, undefined);
